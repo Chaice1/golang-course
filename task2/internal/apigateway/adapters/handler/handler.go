6e51aa096d8fe2ec42a2afd1c92b844e600ff68b
Kyriell <kirya.shmidt.0600@mail.ru>
2026-03-21 23:26:29 +0300

    feat:add structures for responces, add comments for swagger in Handler and params in request's URL

--- task2/internal/apigateway/adapters/handler/handler.go
+++ task2/internal/apigateway/adapters/handler/handler.go
@@ -4,7 +4,7 @@ import (
 	"context"
 	"net/http"
 
-	apigatewayrepoinfomodel "github.com/Chaice1/golang-course/task2/internal/apigateway/adapters/repoinfomodel"
+	apigatewaymodel "github.com/Chaice1/golang-course/task2/internal/apigateway/adapters/model"
 	apigatewaydomain "github.com/Chaice1/golang-course/task2/internal/apigateway/domain"
 	"github.com/gin-gonic/gin"
 )
@@ -22,44 +22,58 @@ func NewApiGatewayHandler(uag UsecaseApiGateway) *apigatewayHandler {
 	}
 }
 
+// @Summary      Get Repository Info
+// @Description  Returns information about a  GitHub repository with the help of Collector
+// @Tags         Gateway
+// @Accept       json
+// @Produce      json
+// @Param        owner  path string  true  "GitHub Owner Name"
+// @Param        repo   path string  true  "Repository Name"
+// @Success      200 {object} apigatewaymodel.SuccessResponce "success"
+// @Failure      404 {string} string "NOT_FOUND"
+// @Failure      500 {string} string "INTERNAL_ERROR"
+// @Failure      400 {string} string "BAD_REQUEST"
+// @Router       /get_repo_info/{owner}/{repo} [get]
 func (agh *apigatewayHandler) GetRepoInfo(c *gin.Context) {
-	var req apigatewayrepoinfomodel.GetRepoInfoReq
-	if err := c.ShouldBindJSON(&req); err != nil {
-		c.JSON(http.StatusBadRequest, gin.H{
-			"message": apigatewaydomain.BadRequest,
+
+	owner := c.Param("owner")
+	repo := c.Param("repo")
+	if len(owner) == 0 || len(repo) == 0 {
+		c.JSON(http.StatusBadRequest, apigatewaymodel.ErrorResponce{
+			Message: apigatewaydomain.BadRequest.Error(),
 		})
 		return
 	}
 
-	resp, err := agh.uag.GetInfoRep(c.Request.Context(), req.Owner, req.Repo)
+	resp, err := agh.uag.GetInfoRep(c.Request.Context(), owner, repo)
 
-	var httpstatus int
+	httpstatus := http.StatusInternalServerError
 
 	switch err {
 	case apigatewaydomain.BadRequest:
 		httpstatus = http.StatusBadRequest
 	case apigatewaydomain.NotFound:
 		httpstatus = http.StatusNotFound
-	case apigatewaydomain.InternalError:
-		httpstatus = http.StatusInternalServerError
 	}
 
 	if err != nil {
-		c.JSON(httpstatus, gin.H{
-			"message": err.Error(),
+		c.JSON(httpstatus, apigatewaymodel.ErrorResponce{
+			Message: err.Error(),
 		})
 		return
 	}
 
-	c.JSON(http.StatusOK, gin.H{
-		"message": "repo is found successfully",
-		"repoinfo": apigatewayrepoinfomodel.GetRepoInfoResp{
-			FullName:    resp.FullName,
-			Description: resp.Description,
-			Stargazers:  resp.Stargazers,
-			Forks:       resp.Forks,
-			CreatedAt:   resp.CreatedAt,
-		},
+	responce := apigatewaymodel.GetRepoInfoResp{
+		FullName:    resp.FullName,
+		Description: resp.Description,
+		Stargazers:  resp.Stargazers,
+		Forks:       resp.Forks,
+		CreatedAt:   resp.CreatedAt,
+	}
+
+	c.JSON(http.StatusOK, apigatewaymodel.SuccessResponce{
+		Message: "success",
+		RepInfo: responce,
 	})
 
 }
