e6f3d2083aea9afeb8cd34edae06b1a886ba1341
Kyriell <kirya.shmidt.0600@mail.ru>
2026-04-09 19:42:39 +0300

    feat:add processor internal

---
+++ task3/repo-stat/processor/internal/domain/domain.go
@@ -0,0 +1,29 @@
+package processor_domain
+
+import (
+	"context"
+	"errors"
+)
+
+type RepoInfo struct {
+	FullName    string
+	Description string
+	Stargazers  uint64
+	Forks       uint64
+	CreatedAt   string
+}
+
+type Ping struct {
+	Reply string
+}
+
+var (
+	ErrNotFound      = errors.New("NOT_FOUND")
+	ErrInternalError = errors.New("INTERNAL_ERROR")
+	ErrBadRequest    = errors.New("BAD_REQUEST")
+)
+
+type CollectorClient interface {
+	GetRepoInfo(context.Context, string, string) (*RepoInfo, error)
+	Ping(context.Context) (*Ping, error)
+}
